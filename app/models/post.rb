c72cee637551b4fd47a9705160bc4ef14bf36cc2
Álvaro Durán Tovar <hermesdt@gmail.com>
2011-08-08 21:01:05 +0200

    Modificado el modelo de post. Modificada vista de post. Modificado estilos para mejorar las tablas

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  belongs_to :author, :class_name => "User", :foreign_key => :author_id
+  
+  validates :title, :uniqueness => true
+  validates :title, :author, :text, :presence => true
 end
